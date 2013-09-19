5a7cfcd65bf40056a9d6637c857f9a0293bebbb3
abrosen <rosen@cs.gsu.edu>
2013-09-19 14:37:45 -0400

    almost done

--- numhw1.py
+++ numhw1.py
@@ -1,7 +1,7 @@
 # Andrew 
 from math import *
 import sys
-
+import scipy.misc.derivative as df
 error =  sys.float_info.epsilon * 10    # close enough that errors won't be my fault
 
 def bisection(func, a, b):
@@ -18,12 +18,44 @@ def bisection(func, a, b):
             #signs are different
             return bisection(func, a, p)
 
+def newtons(f, guess):
+    try:
+        delta = f(guess)/drink(f,guess)
+    except ZeroDivisionError:
+        return guess
+    if fabs(delta) < error:
+        return guess
+    else:
+        return newtons(f, guess - delta)
+
+def newton(f, guess):
+    try:
+        delta = f(guess)/df(f,guess)
+    except ZeroDivisionError:
+        return guess
+    if fabs(delta) < error:
+        return guess
+    else:
+        return newtons(f, guess - delta)
+
+# the hackiest hack that ever hacked
+# don't drink and (approximatly) derive
+def drink(f, x):
+    p0 = x - error
+    p1 = x + error
+    slope =  (f(p1)-f(p0))/(2*error)
+    print p0,p1,(f(p1)-f(p0))
+    return slope
+
 #each of the 6 problems as tuple
 problems= [(lambda x: 3 * x - exp(x), 1.0 , 2.0), 
 (lambda x: x + 3 * cos(x) - exp(x), 0, 1),  
 (lambda x : x**2 -4*x + 4 - log(x), 1, 2), (lambda x : x**2 -4*x + 4 - log(x), 2, 4),
 (lambda x: x + 1 - 2*sin(pi*x), 0, 0.5), (lambda x: x + 1 - 2*sin(pi*x), 0.5, 1)]
 
-results =  map(lambda x: bisection(x[0],x[1],x[2]), problems)
-for result in results:
-    print result
+#results =  map(lambda x: bisection(x[0],x[1],x[2]), problems)
+#for result in results:
+#    print result
+
+
+print newtons(lambda x: x, 2000)
