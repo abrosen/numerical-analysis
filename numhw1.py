1bfb868dc05f2ef9d6fa1a9442e7360c63281f65
abrosen <rosen@cs.gsu.edu>
2013-09-24 16:13:12 -0400

    Update numhw1.py

--- numhw1.py
+++ numhw1.py
@@ -1,7 +1,10 @@
-# Andrew 
+# Andrew Rosen
+# Numerical Analysis Programming Project 1
+# Tested on python 2.7.3
+# Usage: python hw1.py
+
 from math import *
 import sys
-import scipy.misc.derivative as df
 error =  sys.float_info.epsilon * 10    # close enough that errors won't be my fault
 
 def bisection(func, a, b):
@@ -18,44 +21,40 @@ def bisection(func, a, b):
             #signs are different
             return bisection(func, a, p)
 
-def newtons(f, guess):
+def newton(f,df,guess):
     try:
-        delta = f(guess)/drink(f,guess)
+        delta = float(f(guess))/float(df(guess))
     except ZeroDivisionError:
         return guess
     if fabs(delta) < error:
         return guess
     else:
-        return newtons(f, guess - delta)
-
-def newton(f, guess):
-    try:
-        delta = f(guess)/df(f,guess)
-    except ZeroDivisionError:
-        return guess
-    if fabs(delta) < error:
-        return guess
-    else:
-        return newtons(f, guess - delta)
-
-# the hackiest hack that ever hacked
-# don't drink and (approximatly) derive
-def drink(f, x):
-    p0 = x - error
-    p1 = x + error
-    slope =  (f(p1)-f(p0))/(2*error)
-    print p0,p1,(f(p1)-f(p0))
-    return slope
+        return newton(f, df, guess - delta)
 
 #each of the 6 problems as tuple
+#exp(x) ~ e**x
+#log(x) with no base defaults to natural log
 problems= [(lambda x: 3 * x - exp(x), 1.0 , 2.0), 
 (lambda x: x + 3 * cos(x) - exp(x), 0, 1),  
 (lambda x : x**2 -4*x + 4 - log(x), 1, 2), (lambda x : x**2 -4*x + 4 - log(x), 2, 4),
 (lambda x: x + 1 - 2*sin(pi*x), 0, 0.5), (lambda x: x + 1 - 2*sin(pi*x), 0.5, 1)]
 
-#results =  map(lambda x: bisection(x[0],x[1],x[2]), problems)
-#for result in results:
-#    print result
+problems_2 = [
+(lambda x: x**3  - 2*(x**2) - 5, lambda x: 3*(x**2) - 4*x, 1,4),
+(lambda x: x**3 + 3*(x**2) - 1 , lambda x: 3*(x**2) + 6*x,-3, -2),
+(lambda x: x - cos(x)          , lambda x: 1 + sin(x), 0, pi/2 ),
+(lambda x: x - 0.8 - 0.2*sin(x), lambda x: 1 - 0.2*cos(x), 0,pi/2)]
 
 
-print newtons(lambda x: x, 2000)
+results =  map(lambda x: bisection(x[0],x[1],x[2]), problems)
+print "System epsilon is ", sys.float_info.epsilon
+print "bisection examples:"
+for result in results:
+    print result
+
+print "\n\n\n"
+
+print "Newton's examples:"
+blahs = map(lambda x: newton(x[0], x[1],(x[2]+x[3])/2) , problems_2)
+for blah in blahs:
+    print blah
