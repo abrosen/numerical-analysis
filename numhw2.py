1e3d8cd576e2953a91ab28d70538b3c85ce09d1f
abrosen <rosen@cs.gsu.edu>
2013-10-28 19:45:09 -0400

    done

--- numhw2.py
+++ numhw2.py
@@ -1,36 +1,61 @@
+#Andrew Rosen
+
 from math import *
 
-def computeCoeffs(x, f):
-    diffs = divDiffs(x,f)
+def computeCoeffs(x, y):
+    diffs = divDiffs(x,y)
     coeffs = []
     for diff in diffs:
-        print diff
         coeffs.append(diff[0])
     return coeffs
     
-def evalP(x, coeffs, target):
-    terms = [1.0] * len(x)
-    for i in range(1, len(terms)):
-        terms[i] = terms[i-1] * (target - x[i-1])
-    return  reduce(lambda a,b: a+b , map(lambda a, b: a*b, coeffs, terms))
-"""
-def divDiff(x, f):
-    if len(x) == 1:
-        return f(x[0])
-    return  (divDiff(x[1:],f) - divDiff(x[: len(x)-1], f))/ (x[len(x)-1] - x[0])
-"""
-def divDiffs(x,f):
+    
+
+def divDiffs(x,y):
     diffs = []
     for i in range(0, len(x)):
         diffs.append([None] * (len(x)-i))
     for i, col in enumerate(diffs):
         if i == 0 :
-            diffs[i] = map(f,x)
+            diffs[i] = y
         else:
             for j in range(0, len(col)):
                 col[j] = (diffs[i-1][j+1] - diffs[i-1][j])/(x[j+i] - x[j])
+                col[j] = (diffs[i-1][j+1])/(x[j+i] - x[j]) - (diffs[i-1][j])/(x[j+i] - x[j])
             diffs[i] =  col
     return diffs
+        
+    
+    
+def dDiff(x,y):
+    F=[[None for n in xrange(len(x))] for n in xrange(len(x))]
+    for a in range(0,len(x)):
+        F[a][0] = y[a]
+    for i in range(1, len(x)):
+        for j in range(1,i+1):
+            F[i][j] = (F[i][j-1] - F[i-1][j-1])/(x[i] - x[i-j])
+    ret = []
+    for i in range(0, len(x)):
+        ret.append(F[i][i])
+    return ret    
+    
+    
+def evalP(x, coeffs, target):
+    terms = [1.0] * len(x)
+    for i in range(1, len(terms)):
+        terms[i] = terms[i-1] * (target - x[i-1])
+    return   sum(map(lambda a, b: a*b, coeffs, terms))
+
+def evalP2(x, coeffs, target):
+    p = coeffs[0]
+    for i in range(1,len(coeffs)):
+        term = 1.0
+        for j in range(0,i):
+            term = term * (target - x[j])
+        p = p + coeffs[i] * term
+    return p
+
+    
     
 def chebyZeros(num):
     zeros = []
@@ -38,13 +63,24 @@ def chebyZeros(num):
         zeros.append(cos(pi*((2*k - 1.0)/(2*num))))
     return zeros
 
-arr=[]
-for j in  range(0,21):
-    arr.append(-1 + j * .1 )
-f = lambda x: 1/(1 +( 25*x**2))
+
+
+f = lambda x: (1 +( 25* x** 2))**(-1)
 target = .985
 
-coeffs = computeCoeffs(arr, f) 
-p = evalP(arr, coeffs, target)
-print f(target), p
+x1 = []
+for j in  range(0,21):
+    x1.append(-1 +(j/10.0))
+x2 = chebyZeros(21)    
 
+y1 = map(f, x1)
+y2 = map(f, x2)
+
+
+ans = f(target)
+calc1 = evalP(x1,computeCoeffs(x1,y1),target)
+calc2 = evalP(x2,computeCoeffs(x2,y2),target)
+
+print "f(x):       ", ans
+print "Calculated: ", calc1, calc2
+print "Error:     ", fabs(calc1 - ans), fabs(calc2 - ans) 
