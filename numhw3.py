1958eb02db4aa272112c6588227b34150be9039a
abrosen <rosen@cs.gsu.edu>
2014-07-05 16:06:13 -0400

    ?

--- numhw3.py
+++ numhw3.py
@@ -8,25 +8,24 @@ from math import *
 
 def extrapolate(f,a,b, depth):
     R = []
-    h = b-a
     for i in range(0, depth):
         R.append([None]*(depth))
-    R[0][0] = (h/2.0) * (f(a)+ f(b))
+    R[0][0] = ((b-a)*.5)*(f(a)+ f(b))
     for n in range(1, depth):
-        h = (b-a)/(2.0**n)
+        h_n = (b-a)/(2.0**(n))
         points = [0]
-        for k in range(1,int(2**(n-1))):
-            point = f(a + (2*k -1)*h) 
+        for k in range(1,int(2**(n-1)) +1):
+            point = f(a + (2*k -1)*h_n) 
             points.append(point)
-        #print R
-        R[n][0] = .5*R[n-1][0]+h*sum(points)
-    for m,col in enumerate(R):
-        if m == 0:
-            continue
-        for n, row in enumerate(R[n]):
-            R[n][m] =  (1.0/((4.0**m)-1))*(4**m *R[n][m-1] -R[n-1][m-1])
-    print R[depth-1][depth-1]
+        R[n][0] = .5*R[n-1][0]+h_n*sum(points)
+    for m in range(1,depth):
+        for n in range(m,depth):
+            #R[n][m] =  (1.0/((4.0**m)-1))*(4**m *R[n][m-1] -R[n-1][m-1])
+            R[n][m] = R[n][m-1] + (1.0/((4.0**m) - 1.0)) * (R[n][m-1] - R[n-1][m-1]) 
+    #for row in R:
+    #    print row
+    print R[depth-2][depth-2]
     
     
 #try increasing the last argument to increase accuracy
-extrapolate(lambda x: (1/(sqrt(2.0*pi)))*exp(-0.5*x*x)  ,-1,1,5)
+extrapolate(lambda x: (1.0/(sqrt(2.0*pi)))*exp(-0.5*x*x)  ,-1.0,1.0,5)
